f2e54c3a1c2d2234eacb815b6a6bc2445e8bcf18
antoine <aassali.dev@gmail.com>
2026-01-03 21:18:55 +0100

    refactor

--- src/main/kotlin/model/OrderItem.kt
+++ src/main/kotlin/model/OrderItem.kt
@@ -1,23 +1,14 @@
 package com.aassalax.model
 
-import com.aassalax.tax.IMPORT_TAX_RATE
-import com.aassalax.tax.TaxRounder
-import com.aassalax.tax.VAT_RATE
+import com.aassalax.tax.Taxes
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 class OrderItem(val product: Product, val quantity: Int) {
-    fun tax() : BigDecimal {
-        val vat = if(product.category == ProductCategory.BOOK || product.category == ProductCategory.FOOD) BigDecimal(0.0)
-        else TaxRounder.round(product.price * VAT_RATE).multiply(BigDecimal(quantity))
-
-        val importTax = if(product.imported) TaxRounder
-            .round(product.price * IMPORT_TAX_RATE)
-            .multiply(BigDecimal(quantity))
-        else BigDecimal.ZERO
-
-        return vat.add(importTax)
-    }
+    fun tax() : BigDecimal = Taxes.entries
+        .fold(BigDecimal.ZERO) {
+            acc, tax -> acc + tax.computeTaxFor(product).multiply(BigDecimal(quantity))
+        }
 
     fun ttc() : BigDecimal {
         return product.price
