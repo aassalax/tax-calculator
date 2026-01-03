06d1d4744acb5f1e3a91fb2407d0996a09278e0e
antoine <aassali.dev@gmail.com>
2026-01-03 19:49:27 +0100

    Food product should be vat exempted - GREEN

--- src/main/kotlin/OrderItem.kt
+++ src/main/kotlin/OrderItem.kt
@@ -2,7 +2,7 @@ package com.aassalax
 
 class OrderItem(val product: Product, val quantity: Int) {
     fun tax() : Double {
-        return if(product.category == ProductCategory.BOOK) 0.0
+        return if(product.category == ProductCategory.BOOK || product.category == ProductCategory.FOOD) 0.0
         else VAT_RATE
     }
 }
