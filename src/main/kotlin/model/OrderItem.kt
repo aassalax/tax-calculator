09954ec140ced9927f1c3911b4a7400b34266122
antoine <aassali.dev@gmail.com>
2026-01-06 16:07:35 +0100

    Refactor tax computation in `OrderItem` by extracting logic into a private `tax()` method for improved clarity and reuse.

--- src/main/kotlin/model/OrderItem.kt
+++ src/main/kotlin/model/OrderItem.kt
@@ -8,14 +8,14 @@ data class OrderItem(val product: Product, val quantity: Int) {
         get() = Price.from(product.price * quantity)
 
     val totalTax: Price
-        get() = Price.of(Taxes.entries
-            .fold(BigDecimal.ZERO) { acc, tax ->
-                acc + (tax.computeTaxFor(product) * quantity).asMoney()
-            }
-        )
+        get() = Price.of(tax())
 
     val totalTtc: Price
         get() = totalPrice + totalTax
+
+    private fun tax(): BigDecimal = Taxes.entries.fold(BigDecimal.ZERO) { acc, tax ->
+        acc + (tax.computeTaxFor(product) * quantity).asMoney()
+    }
 
     override fun toString(): String {
         return "$quantity $product"
